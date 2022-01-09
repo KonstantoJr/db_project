2e0225ca297fc6a372dd94692c03d8400aab5426
KonstantoJr <konstantokon29@gmail.com>
2022-01-09 19:22:13 +0200

    Add files via upload

--- admin_menu.py
+++ admin_menu.py
@@ -1,2 +1,28 @@
+import sqlite_conection
+
+
+class Admin:
+    def __init__(self) -> None:
+        self.option = self.login()
+
+        if self.option == "1":
+            self.food_appl()
+        elif self.option == "2":
+            self.housing_appl()
+
+    def login(self):
+        print(
+            f"""Press 1 to check food applications\
+        \nPress 2 to check housing applications"""
+        )
+        return input()
+
+    def food_appl(self):
+        pass
+
+    def housing_appl(self):
+        pass
+
+
 if __name__ == "__main__":
-	pass
+    menu = Admin()
